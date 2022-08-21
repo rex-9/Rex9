6cb55be36f57193ab31f53dd3479cf811edebab6
Htet Naing <htetnaing0814@gmail.com>
2022-08-21 20:41:52 +0630

    add project cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,29 @@
 /* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-unused-vars */
 
 import PropTypes from 'prop-types';
 import style from './Card.module.scss';
 
 const Card = (props) => {
   const {
-    image, name, techs, detail, live, source,
+    image, name, techs, brief, detail, live, source,
   } = props.project;
   return (
     <>
       <div className={style.card}>
         <img src={image} alt="" />
         <div className={style.content}>
-          <div>{name}</div>
-          {
-            techs.map((tech) => <div key={tech}>{tech}</div>)
-          }
-          <div>{detail}</div>
-          <a href={source}>Source</a>
-          <a href={live}>Live</a>
+          <div className={style.name}>{name}</div>
+          <div className={style.techsBox}>
+            {
+              techs.map((tech) => <div key={tech} className={style.tech}>{tech}</div>)
+            }
+          </div>
+          <div className={style.brief}>{brief}</div>
+          <div className={style.buttons}>
+            <a href={source}>Source</a>
+            <a href={live}>Live</a>
+          </div>
         </div>
       </div>
     </>

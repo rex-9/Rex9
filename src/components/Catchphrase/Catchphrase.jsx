bb65299b66e95a61c405c0908f375939c7285091
Oga-tsukimori <oga2aoi1500@gmail.com>
2023-08-05 15:44:25 +0630

    change catchphrase

--- src/components/Catchphrase/Catchphrase.jsx
+++ src/components/Catchphrase/Catchphrase.jsx
@@ -7,7 +7,7 @@ const Catchphrase = () => (
     <div className={style.phrase}>
       <p>Greetings, Champion...</p>
       <p>This is Rex9, the Spiritual Full-Stack Developer. ✨</p>
-      <p>I can help you build attractive websites with maximum quality within the shortest time. 😎</p>
+      <p>Let's build something! 😎</p>
     </div>
     <div className={style.profiles}>
       {
